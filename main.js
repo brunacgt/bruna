9527e36bf19b90138a67be1d746384004ae4ea22
brunacgt <117641824+brunacgt@users.noreply.github.com>
2023-11-09 09:20:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+function tocaSom(idElementoAudio){
+  document.querySelector(idElementoAudio).play()
+}
 
+const listadeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+for (let contador = 0; contador < listaDeTeclas.lenght; contador++) {
+
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio ='#som_${instrumento}';
