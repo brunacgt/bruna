29d43dcc7eeaaac399514f6039d99eaf43cacd95
brunacgt <117641824+brunacgt@users.noreply.github.com>
2023-11-09 09:58:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -10,4 +10,10 @@ for (let contador = 0; contador < listaDeTeclas.lenght; contador++) {
 
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
-  const idAudio ='#som_${instrumento}';
+  const idAudio ='#som_${instrumento}';//template string
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  }
+
+}
