6415d9dd192f20b3944b4ad5ed9c2d076721e906
brunacgt <117641824+brunacgt@users.noreply.github.com>
2023-11-23 10:12:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function tocaSom(idElementoAudio){
-  document.querySelector(idElementoAudio).play()
+const elemento= document.querySelector(SeletorAudio);
+ if(elemento && elemento.localName==='audio'){
+   elemento.play();
+ }
+   else{
+  alert('Elemento não encontrado ou seletor inválido');
 }
 
 const listadeTeclas = document.querySelectorAll('.tecla');
@@ -15,5 +20,16 @@ for (let contador = 0; contador < listaDeTeclas.lenght; contador++) {
   tecla.onclick = function () {
     tocaSom(idAudio);
   }
+
+   tecla.onkeydown = function (evento) {
+        
+        if (evento.code=== ‘Enter’ ||evento.code==='space') {
+        tecla.classList.add(‘ativa’);
+        }
+        }
+    
+    tecla.onkeyup = function () {
+        tecla.classList.remove(‘ativa’);
+        }    
 
 }
